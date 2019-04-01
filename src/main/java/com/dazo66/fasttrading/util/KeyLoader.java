b0c8f13cf656b8de79b98fb36ddc4fb0bc45eb7a
Aye10032 <aye10032@gmail.com>
2019-04-01 12:36:19 +0800

    修复Keyboard Input

--- src/main/java/com/dazo66/fasttrading/util/KeyLoader.java
+++ src/main/java/com/dazo66/fasttrading/util/KeyLoader.java
@@ -2,7 +2,7 @@ package com.dazo66.fasttrading.util;
 
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
-import org.lwjgl.input.Keyboard;
+import net.minecraft.client.util.InputMappings;
 
 /**
  * @author Dazo66
@@ -11,7 +11,7 @@ public class KeyLoader {
     public static KeyBinding key_F4;
 
     public KeyLoader() {
-        KeyLoader.key_F4 = new KeyBinding("FastTrading ON-OFF", Keyboard.KEY_F4, "FastTrading");
+        KeyLoader.key_F4 = new KeyBinding("FastTrading ON-OFF", InputMappings.getInputByName("key.keyboard.f4").getKeyCode(), "FastTrading");
         ClientRegistry.registerKeyBinding(KeyLoader.key_F4);
     }
 }
